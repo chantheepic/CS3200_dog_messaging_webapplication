6f9c8dcef72f92e4c22f20d1cfe36848d916040d
ChanMin <park.c@husky.neu.edu>
2019-03-25 14:20:54 -0400

    reverted

---
+++ src/config/dog_database.php
@@ -0,0 +1,20 @@
+<?php
+class DogDatabase{
+    private $servername = "chanminis.online";
+    private $username = "chanmini_dog_api";
+    private $password = "cascade";
+    private $dbname = "chanmini_cs3200_dog";
+    private $conn = null;
+
+    public function connect() {
+        try {
+            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e) {
+            echo "connection failed ". $e->getMessage(). "<br>";
+        }
+        return $this->conn;
+    }
+}
+?>
