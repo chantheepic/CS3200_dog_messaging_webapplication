de2c153141046a4839edc3cfa3e0ff73cb5dfa5e
Tyler Seplak <tyler.j.seplak@gmail.com>
2019-04-18 07:37:30 -0400

    Update inserts to reflect new table format

--- src/insert_statements.sql
+++ src/insert_statements.sql
@@ -28,9 +28,9 @@ INSERT INTO user(name, username, password, gender, zipcode, city) VALUES
 -- Dog
 -- weight in lbs
 INSERT INTO dog(user_id, breed_id, dog_name, fixed, weight, gender, description) VALUES
-(1, 4, 'Max', 1, 60, 'Male', 'Playful adult Akita called Max'), 
+(1, 4, 'Max', 1, 60, 'Male', 'Playful adult Akita called Max'),
 (1, 6, 'Loki', 1, 50, 'Male', 'Adorable little pup'),
-(2, 7,  'Luna', 0, 65, 'Female', 'Looking to make friends'), 
+(2, 7,  'Luna', 0, 65, 'Female', 'Looking to make friends'),
 (3, 24, 'Tucker', 0, 24, 'Male', 'Hi!, I am Tucker'),
 (4, 24, 'Bella', 1, 20, 'Female', 'Hi, I am Bella'),
 (5, 77, 'Molly', 1, 20, 'Female', 'Puppy looking for friends');
@@ -52,16 +52,16 @@ INSERT INTO dog_has_temperament(dog_id, temperament_id) VALUES
 
 -- Photo
 INSERT INTO photo(dog_id, photo, caption) VALUES
-(1, 'C:\Users\mahwa\Downloads\akita.jpg', 'Out for a walk'),
-(1, 'C:\Users\mahwa\Downloads\akita.jpg', 'Playing'),
-(2, 'C:\Users\mahwa\Downloads\malamute.jpg', 'Training'),
-(3, 'C:\Users\mahwa\Downloads\bulldog.jpg', 'Day out'),
-(4, 'C:\Users\mahwa\Downloads\beagle2.JPG', 'Day out'),
-(5, 'C:\Users\mahwa\Downloads\beagle1.jpg', 'Playing'),
-(6, 'C:\Users\mahwa\Downloads\dalmation.jpeg', 'First walk outside');
+(1, 'C:/Users/mahwa/Downloads/akita.jpg', 'Out for a walk'),
+(1, 'C:/Users/mahwa/Downloads/akita.jpg', 'Playing'),
+(2, 'C:/Users/mahwa/Downloads/malamute.jpg', 'Training'),
+(3, 'C:/Users/mahwa/Downloads/bulldog.jpg', 'Day out'),
+(4, 'C:/Users/mahwa/Downloads/beagle2.JPG', 'Day out'),
+(5, 'C:/Users/mahwa/Downloads/beagle1.jpg', 'Playing'),
+(6, 'C:/Users/mahwa/Downloads/dalmation.jpeg', 'First walk outside');
 
 -- Seen
-INSERT INTO seen(user_id, seen_user_id, liked, ts) VALUES
+INSERT INTO seen(dog_id, seen_dog_id, liked, ts) VALUES
 (1, 2, 1, '2019-03-28 16:43:56'),
 (1, 3, 1, '2019-03-26 11:35:45'),
 (2, 3, 1, '2019-04-01 18:23:26'),
@@ -70,7 +70,7 @@ INSERT INTO seen(user_id, seen_user_id, liked, ts) VALUES
 (4, 2, 0, '2019-03-20 10:15:32');
 
 -- Pal
-INSERT INTO pal(user1, user2) VALUES
+INSERT INTO pal(dog_id1, dog_id2) VALUES
 (3, 1),
 (2, 3);
 
@@ -89,8 +89,8 @@ INSERT INTO session(session_id, user_id, session_start) VALUES
 (6, 2, '2019-04-01 18:22:45');
 
 -- Message
-INSERT INTO message(time_sent, content, sender_id, pal_id) VALUES
-('2019-03-27 10:38:43', 'Hi Alex! I am Tim. When can Tucker and Max meet?', 3, 1),
-('2019-03-27 16:23:33', 'Hi Tim! We are available tomorrow at 2 pm', 1, 1),
-('2019-04-01 18:25:00', 'Hello. Luna is excited to meet Tucker!', 2, 2),
-('2019-04-02 13:50:16', 'Hi Sarah!', 3, 2);
+INSERT INTO message(time_sent, content, flag, dog_id1, dog_id2) VALUES
+('2019-03-27 10:38:43', 'Hi Alex! I am Tim. When can Tucker and Max meet?', 1, 3, 1),
+('2019-03-27 16:23:33', 'Hi Tim! We are available tomorrow at 2 pm', 0, 1, 1),
+('2019-04-01 18:25:00', 'Hello. Luna is excited to meet Tucker!', 1, 2, 2),
+('2019-04-02 13:50:16', 'Hi Sarah!', 0, 3, 2);
