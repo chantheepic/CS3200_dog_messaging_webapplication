f125697372ce0a4d5ad4cb1117b3e85e0a9052bf
pahalsinghania <pahalsinghania@gmail.com>
2019-04-21 16:56:59 -0400

    matches display

--- src/dog_procedures.sql
+++ src/dog_procedures.sql
@@ -44,9 +44,6 @@ SELECT @message;
 CALL RegisterDog(2, 'Akita', 'Dirk', 1, 40, 'Female', 'Dirk is dog', @message);
 CALL RegisterDog(78, 'Akita', 'AAA', 1, 40, 'Male', 'AAA is a dog', @message);
 
-SELECT * FROM dog;
-SELECT * FROM user;
-
 DROP PROCEDURE IF EXISTS retreiveUserDogs;
 # Retreive dogs
 DELIMITER //
@@ -72,14 +69,16 @@ CREATE PROCEDURE retreiveDogPals(userId INT(10), dogId INT(10))
     SELECT dog_id 
     INTO auth_dog_id
     FROM dog 
-    WHERE user_id = userId AND dog_id = dogID;
+    WHERE user_id = userId AND dog_id = dogId;
 	
-    SELECT d.dog_id, d.dog_name
-    FROM pal p join dog d on (p.dog1 = d.dog_id)
+    SELECT d.dog_id, d.dog_name, p1.photo_id
+    FROM pal p join dog d on (p.dog1 = d.dog_id) 
+		join photo p1 on (p1.dog_id = d.dog_id)
     WHERE (p.dog2 = auth_dog_id)
     UNION
-	SELECT d.dog_id, d.dog_name
-    FROM pal p join dog d on (p.dog2 = d.dog_id)
+	SELECT d.dog_id, d.dog_name, p1.photo_id
+    FROM pal p join dog d on (p.dog2 = d.dog_id) 
+		join photo p1 on (p1.dog_id = d.dog_id)
     WHERE (p.dog1 = auth_dog_id);
 
   END //
@@ -109,6 +108,27 @@ CREATE PROCEDURE block(dog1_id INT(10), dog2_id INT(10))
     DELETE 
     FROM pal 
 	WHERE (dog1 = dog1_id && dog2 = dog2_id) || (dog2 = dog1_id && dog1 = dog2_id);
+
+  END //
+DELIMITER ;
+
+# Delete Dog
+DROP PROCEDURE IF EXISTS deleteDog;
+DELIMITER //
+CREATE PROCEDURE deleteDog(userId INT(10), dogId INT(10))
+  BEGIN
+
+    DECLARE auth_dog_id INT;
+    
+    SELECT dog_id 
+    INTO auth_dog_id
+    FROM dog d
+    WHERE d.user_id = userId AND dog_id = dogId;
+
+
+    DELETE 
+    FROM dog 
+	WHERE dog_id = auth_dog_id;
 
   END //
 DELIMITER ;
--- src/recommendations.sql
+++ src/recommendations.sql
@@ -1,110 +1,88 @@
 # Dog Recommendations - Returns 10 recommended dogs
 
-DELIMITER //
 DROP PROCEDURE IF EXISTS RecommendDogs;
-CREATE PROCEDURE RecommendDogs
-(
-    # The dog_id of the dog we are doing a search for
-    IN id INT(3)
-)
-
-   BEGIN
-
+DELIMITER //
+CREATE PROCEDURE RecommendDogs(dogId INT(10))
+  BEGIN
     DECLARE dogs_user_id INT;
     DECLARE dogs_zipcode INT;
 
-	SELECT user_id
-    INTO dogs_user_id
-    FROM dog
-    WHERE dog_id = id;
+	SELECT user_id, zipcode
+    INTO dogs_user_id, dogs_zipcode
+    FROM dog join user using (user_id)
+    WHERE dog_id = dogId;
 
-    SELECT zipcode
-    INTO dogs_zipcode
-    FROM user
-	WHERE user_id = dogs_user_id;
-
-    SELECT UNIQUE dog_id
-    FROM dog JOIN user ON user_id JOIN blocked ON user_id
-    WHERE dog.dog_id != id
-    AND user.zipcode = dogs_zipcode
-    AND dog.user_id NOT IN
-        (SELECT blocker_id, blockee_id
-         FROM blocked
-         WHERE blockee_id = dogs_user_id
-         OR blocker_id = dogs_user_id)
+    SELECT DISTINCT d.dog_id, d.dog_name, d.fixed, b.breed_name, d.gender, d.weight, photo_id
+    FROM dog d JOIN user u using (user_id) 
+		JOIN breed b using (breed_id)
+		join photo p using (dog_id)
+    WHERE d.dog_id != dogId
+    AND d.user_id != dogs_user_id
+    AND ROUND(u.zipcode / 100, 0) = ROUND(dogs_zipcode / 100, 0)
+    AND d.user_id not in 
+		(SELECT blockee_id
+        FROM blocked b
+		WHERE b.blocker_id = dogs_user_id 
+        UNION
+        SELECT blocker_id
+        FROM blocked b
+		WHERE b.blockee_id = dogs_user_id)
+	AND d.dog_id not in 
+		(SELECT seen_user_id
+        FROM seen s
+		WHERE s.user_id = dogs_user_id)
     LIMIT 10;
-
   END //
 DELIMITER ;
-
+    
 # Get Basic Info About Dog
-
-DELIMITER //
 DROP PROCEDURE IF EXISTS GetDogInfo;
-CREATE PROCEDURE GetDogInfo
-(
-	IN id INT(3)
-)
-
- BEGIN
+DELIMITER //
+CREATE PROCEDURE GetDogInfo(id INT(10))
+  BEGIN
 
     SELECT fixed, weight, gender, description
     FROM dog
     WHERE dog_id = id;
 
- END //
+  END //
 DELIMITER ;
 
 # Get Picture(s) of Dog
-
-DELIMITER //
 DROP PROCEDURE IF EXISTS GetDogPictures;
-CREATE PROCEDURE GetDogPictures
-(
-	IN id INT(3)
-)
-
- BEGIN
-
+DELIMITER //
+CREATE PROCEDURE GetDogPictures(id INT(10))
+  BEGIN
+  
 	SELECT photo
     FROM photo
     WHERE dog_id = id;
 
- END //
+  END //
 DELIMITER ;
 
 # Get the temperament(s) of the dog
-
-DELIMITER //
 DROP PROCEDURE IF EXISTS GetDogTemperaments;
-CREATE PROCEDURE GetDogTemperaments
-(
-	IN id INT(3)
-)
-
-BEGIN
-
-    SELECT UNIQUE temp.temperament_name
+DELIMITER //
+CREATE PROCEDURE GetDogTemperaments(id INT(10))
+  BEGIN
+  
+    SELECT DISTINCT temp.temperament_name
     FROM dog_has_temperament JOIN temperament ON temperament_id
     WHERE dog_id = id;
 
- END //
+  END //
 DELIMITER ;
 
 # Get the breed of the dog
-
-DELIMITER //
 DROP PROCEDURE IF EXISTS GetDogBreed;
-CREATE PROCEDURE GetDogBreed
-(
-	IN id INT(3)
-)
-
- BEGIN
-
+DELIMITER //
+CREATE PROCEDURE GetDogBreed(IN id INT(10))
+  BEGIN
+  
     SELECT breed_name
     FROM breed JOIN dog ON breed_id
     WHERE dog_id = id;
 
- END //
+  END //
 DELIMITER ;
