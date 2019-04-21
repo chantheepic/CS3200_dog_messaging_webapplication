a8b9d8ad70330f2d91fe382e7f554b5b89421c0f
pahalsinghania <pahalsinghania@gmail.com>
2019-04-21 08:55:31 -0400

    block users based on dog

--- src/dog_registration_procedure.sql
+++ src/dog_registration_procedure.sql
@@ -39,6 +39,7 @@ CREATE PROCEDURE RegisterDog
   END //
 DELIMITER ;
 
+#test
 SELECT @message;
 CALL RegisterDog(2, 'Akita', 'Dirk', 1, 40, 'Female', 'Dirk is dog', @message);
 CALL RegisterDog(78, 'Akita', 'AAA', 1, 40, 'Male', 'AAA is a dog', @message);
@@ -54,6 +55,8 @@ CREATE PROCEDURE retreiveUserDogs(IN userId INT(10))
     SELECT dog_id, dog_name FROM dog WHERE user_id = userId;
   END //
 DELIMITER ;
+
+#test
 delete from dog where user_id = 78;
 select * from dog;
 CALL retreiveUserDogs(78);
@@ -74,6 +77,35 @@ CREATE PROCEDURE retreiveDogPals(IN userId INT(10), IN dogID INT(10))
     SELECT dog_id, dog_name
     FROM pal
     WHERE (dog1 = authenticatedDog_id || dog2 = authenticatedDog_id)
+
+  END //
+DELIMITER ;
+
+
+# Block users
+DELIMITER //
+DROP PROCEDURE IF EXISTS block;
+CREATE PROCEDURE block(IN dog1_id INT(10), IN dog2_id INT(10))
+  BEGIN
+
+    DECLARE user_id INT;
+    DECLARE blocker_id INT;
+
+    SELECT user_id
+    INTO user_id
+    FROM dog
+    WHERE dog1_id = dog_id;
+
+    SELECT user_id
+    INTO blocker_id
+    FROM dog
+    WHERE dog2_id = dog_id;
+
+    INSERT INTO blocked VALUES(user_id, blocker_id);
+
+    DELETE 
+    FROM pal 
+	WHERE (dog1 = dog1_id && dog2 = dog2_id) || (dog2 = dog1_id && dog1 = dog2_id)
 
   END //
 DELIMITER ;
